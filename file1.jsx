bbdadd88368ccd9a4d2b989cf867ddcdbc5b26f5
saularraffi <saular.raffi@airspacelink.com>
2024-04-24 10:24:59 -0400

    change files

--- file1.jsx
+++ file1.jsx
@@ -10,20 +10,17 @@ const BannerOne = (props) => {
       dimensions="h-[40vw] lg:h-72 w-full"
       className={`rounded-2xl overflow-hidden ${
         index === 0
-          ? "lg:rounded-none lg:rounded-tl-2xl"
+          ? "lg:rounded-none lg:rounded-tl-2xll"
           : index === 1
-          ? "lg:rounded-none lg:rounded-tr-2xl"
+          ? "lg:rounded-none lg:rounded-tr-2xll"
           : index === 2
-          ? "lg:rounded-none lg:rounded-bl-2xl"
-          : "lg:rounded-none lg:rounded-br-2xl"
+          ? "lg:rounded-none lg:rounded-bl-2xll"
+          : "lg:rounded-none lg:rounded-br-2xll"
       }`}
       src={item.image.url}
       alt={item.title}
     />
   );
-
-  //? Render(s)
-  if (data.length === 0) return null;
 
   return (
     <section className="grid gap-3 px-3 lg:relative lg:grid-cols-2 lg:gap-4">
